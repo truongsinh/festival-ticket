1dd36809d7f44b72515d5262601d5f41f801a3b0
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2014-01-03 02:27:06 +0200

    start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
-export NODE_APP_PORT=$1 \
-  && export NODE_ENV=production \
-  && nohup node backend/index.js > log.out 2> log.err < /dev/null &
+
+# move pid if exists
+if [ -f pid ]
+then
+  mv pid pid.old
+fi
+
+# throw error if port invalid
+if [ ! $1 -gt 1023 ]
+then
+  echo 'Port must be greater than 1023' >&2
+  exit 1
+fi
+echo 'continue'
+export NODE_APP_PORT=$1
+export NODE_ENV=production
+nohup node backend/index.js > log.out 2> log.err < /dev/null & echo $! > pid
